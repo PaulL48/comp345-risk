fc3c38a2bed3a2b33dc2c810d16a3f3a16f3aeae
Paul Lane <paul.lane083@gmail.com>
2020-09-23 17:07:27 -0400

    Evolving to iterator implementation. WIP

--- headers/Graph.h
+++ headers/Graph.h
@@ -8,11 +8,15 @@
 #include <ostream>
 #include <type_traits>
 
+template <class T, class M> 
+M get_member_type(M T:: *);
 
-template<typename T>
+template <typename T>
 class Graph
 {
 public:
+    using NeighborType = typename std::remove_pointer<decltype(get_member_type(&Graph<T>::adjacencyList))>::type::mapped_type;
+
     Graph();
     ~Graph();
     Graph(const Graph<T>& graph);
@@ -39,74 +43,104 @@ public:
     void connect(const T& vertex1, const T& vertex2);
 
     void iter_dfs();
+
+    class DepthFirstIterator
+    {
+    public:
+        using NeighborType = typename std::remove_pointer<decltype(get_member_type(&Graph<T>::adjacencyList))>::type::mapped_type;
+        DepthFirstIterator(std::unordered_map<T, std::vector<T>>* adjacencyList);
+        DepthFirstIterator(const DepthFirstIterator& dfi);
+        ~DepthFirstIterator();
+        DepthFirstIterator& operator=(const DepthFirstIterator& dfi);
+        bool operator==(const DepthFirstIterator& dfi) const;
+        bool operator!=(const DepthFirstIterator& dfi) const;
+        DepthFirstIterator& operator++();
+        T& operator*() const;
+        T* operator->() const;
+    private:
+        std::unordered_set<const NeighborType*>* visited;
+        std::stack<T*, std::vector<const T*>>* stack;
+
+        // This is holding a pointer to the adjacency list
+        // The motive for this is there is no implicit way to get
+        // the neighbors of a neighbor. You must directly consult the adj list
+        std::unordered_map<T, std::vector<T>>* adjacencyList;
+    };
+
+    DepthFirstIterator begin();
+    DepthFirstIterator end();
+
+    friend DepthFirstIterator;
 private:
     std::unordered_map<T, std::vector<T>>* adjacencyList;
 };
 
-template<typename T>
+template <typename T>
 Graph<T>::Graph() : adjacencyList(new std::unordered_map<T, std::vector<T>>)
 {
 }
 
-template<typename T>
+template <typename T>
 Graph<T>::~Graph()
 {
     delete this->adjacencyList;
 }
 
-template<typename T>
-Graph<T>::Graph(const Graph<T>& graph)
+template <typename T>
+Graph<T>::Graph(const Graph<T>& graph) : adjacencyList(new std::unordered_map<T, std::vector<T>>)
 {
     *(this->adjacencyList) = *graph.adjacencyList;
 }
 
-template<typename T>
+template <typename T>
 Graph<T>& Graph<T>::operator=(const Graph<T>& graph)
 {
     *(this->adjacencyList) = *graph.adjacencyList;
     return *this;
 }
 
-template<typename T>
+template <typename T>
 std::ostream& operator<<(std::ostream& output, const Graph<T>& graph)
 {
     return output;
 }
 
-template<typename T>
+template <typename T>
 bool Graph<T>::isConnected() const
 {
+    
     return false;
 }
 
-template<typename T>
+template <typename T>
 bool Graph<T>::isSubgraphOf(const Graph<T>& graph) const
 {
     return false;
 }
 
-template<typename T>
+template <typename T>
 void Graph<T>::insert(const T& vertex)
 {
     if (this->adjacencyList->count(vertex) == 0)
     {
-        typename std::remove_pointer<decltype(this->adjacencyList)>::type::mapped_type neighbors;
+        Graph<T>::NeighborType neighbors;
         this->adjacencyList->insert(std::make_pair(vertex, neighbors));
     }
 }
 
-template<typename T>
+template <typename T>
 void Graph<T>::connect(const T& vertex1, const T& vertex2)
 {
     if (this->adjacencyList->count(vertex1) != 0 && this->adjacencyList->count(vertex2) != 0)
     {
         this->adjacencyList->at(vertex1).push_back(vertex2);
+        this->adjacencyList->at(vertex1).push_back(vertex1);
     }
 }
 
 #include <iostream>
 
-template<typename T>
+template <typename T>
 void Graph<T>::iter_dfs()
 {
     std::cout << "Starting DFS" << std::endl;
@@ -117,8 +151,8 @@ void Graph<T>::iter_dfs()
 
     // Instead of storing key addresses, we store value addresses
     // This gives O(1) lookups and uniqueness
-    using mapped_type = typename std::remove_pointer<decltype(this->adjacencyList)>::type::mapped_type;
-    std::unordered_set<const mapped_type*> visited;
+
+    std::unordered_set<const Graph<T>::NeighborType*> visited;
     std::stack<const T*, std::vector<const T*>> stack;
     stack.push(&(this->adjacencyList->cbegin()->first));
 
@@ -143,6 +177,100 @@ void Graph<T>::iter_dfs()
             }
         }
     }
+}
+
+template <typename T>
+typename Graph<T>::DepthFirstIterator Graph<T>::begin()
+{
+    return DepthFirstIterator(this->adjacencyList);
+}
+
+template <typename T>
+typename Graph<T>::DepthFirstIterator Graph<T>::end()
+{
+    return DepthFirstIterator(nullptr);
+}
+
+template <typename T>
+Graph<T>::DepthFirstIterator::DepthFirstIterator(std::unordered_map<T, std::vector<T>>* adjacencyList) : adjacencyList(adjacencyList),
+                                                                                                         visited(new std::unordered_set<const Graph<T>::NeighborType *>()),
+                                                                                                         stack(new std::stack<const T *, std::vector<const T *>>())
+{
+    if (adjacencyList != nullptr)
+    {
+        this->stack->push(&(adjacencyList->begin()->first));
+    }
+}
+
+template <typename T>
+Graph<T>::DepthFirstIterator::DepthFirstIterator(const DepthFirstIterator &dfi) : visited(new std::unordered_set<const Graph<T>::NeighborType *>()),
+                                                                                  stack(new std::stack<const T *, std::vector<const T *>>())
+{
+    this->adjacencyList = dfi.adjacencyList;
+    *this->visited = *dfi.visited;
+    *this->stack = *dfi.stack;
+}
+
+template <typename T>
+Graph<T>::DepthFirstIterator::~DepthFirstIterator()
+{
+    delete this->visited;
+    delete this->stack;
+}
+
+template <typename T>
+typename Graph<T>::DepthFirstIterator &Graph<T>::DepthFirstIterator::operator=(const DepthFirstIterator &dfi)
+{
+    *this->visited = *dfi.visited;
+    *this->stack = *dfi.stack;
+}
+
+template <typename T>
+bool Graph<T>::DepthFirstIterator::operator==(const DepthFirstIterator &dfi) const
+{
+    return *this->stack == *dfi.stack && *this->visited == *dfi.visited;
+}
+
+template <typename T>
+bool Graph<T>::DepthFirstIterator::operator!=(const DepthFirstIterator &dfi) const
+{
+    return !(*this == dfi);
+}
+
+template <typename T>
+typename Graph<T>::DepthFirstIterator &Graph<T>::DepthFirstIterator::operator++()
+{
+    const T* current = this->stack->top();
+    this->stack->pop();
+
+    if (this->visited->count(&(this->adjacencyList->at(*current))) != 0)
+    {
+        return *this;
+    }
+
+    this->visited->insert(&(this->adjacencyList->at(*current)));
+    for (const T& neighbor : this->adjacencyList->at(*current))
+    {
+        if (this->visited->count(&(this->adjacencyList->at(neighbor))) == 0)
+        {
+            this->stack->push(&neighbor);
+        }
+    }
+
+    return *this;
+}
+
+template <typename T>
+T& Graph<T>::DepthFirstIterator::operator*() const
+{
+    // Derefencing at or past end iterator is UB
+    return *(this->stack->top());
+}
+
+template <typename T>
+T* Graph<T>::DepthFirstIterator::operator->() const
+{
+    return this->stack->top();
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,23 @@
 int main()
 {
     Graph<int> g;
+    g.insert(4);
     g.insert(1);
     g.insert(2);
     g.insert(3);
+    
     g.connect(1, 2);
+    g.connect(1, 1);
+    g.connect(1, 4);
     g.connect(1, 3);
     g.connect(2, 3);
     g.connect(3, 1);
+    
 
-    g.iter_dfs();
+    for (auto it = g.begin(); it != g.end(); ++it)
+    {
+        std::cout << *it << std::endl;
+    }
 
     return 0;
 }
