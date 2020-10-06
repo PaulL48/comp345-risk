2ee9fb473bef59b051e2ecbe1d62e3e86eaf0cf5
chaoukis <chaoukisammy@gmail.com>
2020-10-06 00:09:22 -0400

    Add driver and testing

--- headers/Player.h
+++ headers/Player.h
@@ -14,11 +14,10 @@ class Player{
     public:
         Player(std::string* playerName,std::vector<int>* territoriesAttack, std::vector<int>* territoriesDefend, std::vector<int>* cards, std::vector<int>* orders);
         Player(const Player& player);
-        Player& Player::operator= (const Player& player);
+        Player& operator= (const Player& player);
         friend std::ostream& operator<<( std::ostream &output, const Player& player); 
-        Player& operator=(const Player& player);
-        std::vector<int>* getAttackTerritories(){ return territoriesAttack;}
-        std::vector<int>* getDefendTerritories(){ return territoriesDefend;}
+        std::vector<int>* toAttack(){ return territoriesAttack;}
+        std::vector<int>* toDefend(){ return territoriesDefend;}
         std::vector<int>* getCards(){ return cards;}
         std::vector<int>* getOrders(){return orders;}
         std::string* getPlayerName(){return playerName;}
