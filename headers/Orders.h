7ac61545469e668122b747978e39dbc16540d899
Paul Lane <paul.lane083@gmail.com>
2020-10-12 03:18:29 -0400

    Adding formatting file, and formatting code

--- headers/Orders.h
+++ headers/Orders.h
@@ -1,20 +1,20 @@
 #ifndef ORDERS_H
 #define ORDERS_H
-#include <vector>
 #include <iostream>
+#include <vector>
 
 template <typename T>
-void polymorphicDeepCopy(std::vector<T*>& dest, const std::vector<T*>& source)
+void polymorphicDeepCopy(std::vector<T *> &dest, const std::vector<T *> &source)
 {
     // Free previous elements
-    for (T* element : dest)
+    for (T *element : dest)
     {
         delete element;
     }
 
     dest.resize(source.size());
 
-    // Clone polymorphic elements 
+    // Clone polymorphic elements
     for (std::size_t i = 0; i < dest.size(); ++i)
     {
         dest.at(i) = source.at(i)->clone();
@@ -22,9 +22,9 @@ void polymorphicDeepCopy(std::vector<T*>& dest, const std::vector<T*>& source)
 }
 
 template <typename T>
-std::vector<T*> *clone(const std::vector<T*>& vector)
+std::vector<T *> *clone(const std::vector<T *> &vector)
 {
-    std::vector<T*>* result = new std::vector<T*>(vector.size());
+    std::vector<T *> *result = new std::vector<T *>(vector.size());
     for (std::size_t i = 0; i < result->size(); ++i)
     {
         result->at(i) = vector.at(i)->clone();
@@ -33,115 +33,129 @@ std::vector<T*> *clone(const std::vector<T*>& vector)
 }
 
 class Order;
-class OrdersList{
-    public:
-        OrdersList();   //constructor
-        OrdersList(const OrdersList &order); //copy constructor
-        OrdersList& operator = (const OrdersList &order);  //copy assignment operator
-        ~OrdersList(); //destructor
-        friend std::ostream & operator << (std::ostream &out, const OrdersList &ordersList); //stream insertion operator 
-        void addToList(const Order& order); //adds an order to the end of a list
-        void moveToFront(const Order& order); //moves an order to the front of the list
-        void moveToEnd(const Order& order); //moves an order to the end of the list
-        void moveUp(const Order& order); //moves an order up by one position
-        void moveDown(const Order& order); //moves an order down by one position
-        void remove(const Order& order); //removes an order from the list
-        const std::vector<Order*>& getList(); //returns orders vector by const reference
-    
-    private:
-        void move(const Order& order, int index); //private move method used to by public move methods 
-        std::vector<Order*> *orders; //vector pointer to hold Order pointers
+class OrdersList
+{
+public:
+    OrdersList();                                   // constructor
+    OrdersList(const OrdersList &order);            // copy constructor
+    OrdersList &operator=(const OrdersList &order); // copy assignment operator
+    ~OrdersList();                                  // destructor
+    friend std::ostream &
+    operator<<(std::ostream &out,
+               const OrdersList &ordersList); // stream insertion operator
+    void addToList(const Order &order);       // adds an order to the end of a list
+    void moveToFront(const Order &order);     // moves an order to the front of the list
+    void moveToEnd(const Order &order);       // moves an order to the end of the list
+    void moveUp(const Order &order);          // moves an order up by one position
+    void moveDown(const Order &order);        // moves an order down by one position
+    void remove(const Order &order);          // removes an order from the list
+    const std::vector<Order *> &getList(); // returns orders vector by const reference
+
+private:
+    void move(const Order &order,
+              int index);         // private move method used to by public move methods
+    std::vector<Order *> *orders; // vector pointer to hold Order pointers
 };
 
-class Order{
-    public:
-        Order(const std::string& description, const std::string& effect); //constructor
-        virtual ~Order(); //destructor
-        const std::string&  getDescription(); //returns description string by const reference
-        const std::string& getEffect(); //returns effect string by const reference
-        const bool& getExecutedStatus(); //returns executed status bool by const reference 
-        void setExecutedStatus(bool status); //sets executed status
-        friend std::ostream & operator << (std::ostream &out, const Order &order); //stream insertion operator 
-        virtual void validate() = 0; //validates an order, virtual method
-        virtual void execute() = 0; //executes an order, virtual method
-        virtual Order* clone() const = 0; //clones an Order object and returns Order pointer, virtual method
-        int getUniqueId() const;
-        
-    protected:
-        Order(const Order &order); //copy constructor
-        Order& operator = (const Order &order); //copy assignment operator 
-        int* uniqueId;        
-        bool* executed = new bool(false); //initialized to false
-        std::string* description; 
-        std::string* effect;
-        static int counter;
+class Order
+{
+public:
+    Order(const std::string &description,
+          const std::string &effect); // constructor
+    virtual ~Order();                 // destructor
+    const std::string &
+    getDescription();                // returns description string by const reference
+    const std::string &getEffect();  // returns effect string by const reference
+    const bool &getExecutedStatus(); // returns executed status bool by const reference
+    void setExecutedStatus(bool status); // sets executed status
+    friend std::ostream &operator<<(std::ostream &out,
+                                    const Order &order); // stream insertion operator
+    virtual void validate() = 0;      // validates an order, virtual method
+    virtual void execute() = 0;       // executes an order, virtual method
+    virtual Order *clone() const = 0; // clones an Order object and returns Order
+                                      // pointer, virtual method
+    int getUniqueId() const;
 
+protected:
+    Order(const Order &order);            // copy constructor
+    Order &operator=(const Order &order); // copy assignment operator
+    int *uniqueId;
+    bool *executed = new bool(false); // initialized to false
+    std::string *description;
+    std::string *effect;
+    static int counter;
 };
 
-class Deploy: public Order{
-    public: 
-        Deploy();
-        Deploy(const Deploy &order);
-        virtual ~Deploy();
-        Deploy& operator = (const Deploy &order); 
-        void validate();
-        void execute();
-        virtual Order* clone() const;
+class Deploy : public Order
+{
+public:
+    Deploy();
+    Deploy(const Deploy &order);
+    virtual ~Deploy();
+    Deploy &operator=(const Deploy &order);
+    void validate();
+    void execute();
+    virtual Order *clone() const;
 };
 
-class Advance: public Order{
-    public: 
-        Advance();
-        Advance(const Advance &order);
-        virtual ~Advance();
-        Advance& operator = (const Advance &order);
-        void validate();
-        void execute();
-        virtual Order* clone() const;
+class Advance : public Order
+{
+public:
+    Advance();
+    Advance(const Advance &order);
+    virtual ~Advance();
+    Advance &operator=(const Advance &order);
+    void validate();
+    void execute();
+    virtual Order *clone() const;
 };
 
-class Bomb: public Order{
-    public: 
-        Bomb();
-        Bomb(const Bomb &order);
-        virtual ~Bomb();
-        Bomb& operator = (const Bomb &order);
-        void validate();
-        void execute();
-        virtual Order* clone() const;
+class Bomb : public Order
+{
+public:
+    Bomb();
+    Bomb(const Bomb &order);
+    virtual ~Bomb();
+    Bomb &operator=(const Bomb &order);
+    void validate();
+    void execute();
+    virtual Order *clone() const;
 };
 
-class Blockade: public Order{
-    public: 
-        Blockade();
-        Blockade(const Blockade &order);
-        virtual ~Blockade();
-        Blockade& operator = (const Blockade &order);
-        void validate();
-        void execute();
-        virtual Order* clone() const;
+class Blockade : public Order
+{
+public:
+    Blockade();
+    Blockade(const Blockade &order);
+    virtual ~Blockade();
+    Blockade &operator=(const Blockade &order);
+    void validate();
+    void execute();
+    virtual Order *clone() const;
 };
 
-class Airlift: public Order{
-    public: 
-        Airlift();
-        Airlift(const Airlift &order);
-        virtual ~Airlift();
-        Airlift& operator = (const Airlift &order);
-        void validate();
-        void execute();
-        virtual Order* clone() const;
+class Airlift : public Order
+{
+public:
+    Airlift();
+    Airlift(const Airlift &order);
+    virtual ~Airlift();
+    Airlift &operator=(const Airlift &order);
+    void validate();
+    void execute();
+    virtual Order *clone() const;
 };
 
-class Negotiate: public Order{
-    public: 
-        Negotiate();
-        Negotiate(const Negotiate &order);
-        virtual ~Negotiate();
-        Negotiate& operator = (const Negotiate &order);
-        void validate();
-        void execute();
-        virtual Order* clone() const;
+class Negotiate : public Order
+{
+public:
+    Negotiate();
+    Negotiate(const Negotiate &order);
+    virtual ~Negotiate();
+    Negotiate &operator=(const Negotiate &order);
+    void validate();
+    void execute();
+    virtual Order *clone() const;
 };
 
 #endif
--- src/Orders.cpp
+++ src/Orders.cpp
@@ -1,46 +1,59 @@
 #include "Orders.h"
 #include <algorithm>
-using std::vector;
 using std::ostream;
 using std::string;
+using std::vector;
 
-OrdersList::OrdersList(): orders(new vector<Order*>()){}
+OrdersList::OrdersList() : orders(new vector<Order *>())
+{
+}
 
-OrdersList::OrdersList(const OrdersList &ordersList): orders(new vector<Order*>(*ordersList.orders)){}
+OrdersList::OrdersList(const OrdersList &ordersList) :
+    orders(new vector<Order *>(*ordersList.orders))
+{
+}
 
-OrdersList::~OrdersList(){
-    for (std::size_t i = 0; i < orders->size(); ++i){ //deletes order pointers in the vector prior to deleting the vector
+OrdersList::~OrdersList()
+{
+    for (std::size_t i = 0; i < orders->size(); ++i)
+    { // deletes order pointers in the vector prior to deleting the vector
         delete orders->at(i);
     }
     delete this->orders;
 }
 
-OrdersList& OrdersList::operator = (const OrdersList &ordersList){
-    if(this == &ordersList){ 
-        return *this; 
+OrdersList &OrdersList::operator=(const OrdersList &ordersList)
+{
+    if (this == &ordersList)
+    {
+        return *this;
     }
 
-    orders = new vector<Order*>(*ordersList.orders);
+    orders = new vector<Order *>(*ordersList.orders);
     return *this;
-} 
+}
 
-ostream & operator << (ostream &out, const OrdersList &ordersList) 
-{ 
-    for (const Order* order : *ordersList.orders){
+ostream &operator<<(ostream &out, const OrdersList &ordersList)
+{
+    for (const Order *order : *ordersList.orders)
+    {
         out << *order << "\n";
     }
-    return out; 
-} 
+    return out;
+}
 
-
-void OrdersList::addToList(const Order& order){ 
+void OrdersList::addToList(const Order &order)
+{
     orders->push_back(order.clone());
 }
 
-void OrdersList::move(const Order& order, int index){
-    for (std::size_t i = 0; i < orders->size(); ++i){
-        if ((orders->at(i)->getUniqueId()) == (order.getUniqueId())){
-            Order* temp = orders->at(i);
+void OrdersList::move(const Order &order, int index)
+{
+    for (std::size_t i = 0; i < orders->size(); ++i)
+    {
+        if ((orders->at(i)->getUniqueId()) == (order.getUniqueId()))
+        {
+            Order *temp = orders->at(i);
             orders->erase(std::next(orders->cbegin(), i));
             orders->insert(std::next(orders->cbegin(), index), temp);
             break;
@@ -48,67 +61,90 @@ void OrdersList::move(const Order& order, int index){
     }
 }
 
-void OrdersList::moveToFront(const Order& order){
+void OrdersList::moveToFront(const Order &order)
+{
     this->move(order, 0);
 }
 
-void OrdersList::moveToEnd(const Order& order){
-    this->move(order, orders->size()-1);
+void OrdersList::moveToEnd(const Order &order)
+{
+    this->move(order, orders->size() - 1);
 }
 
-void OrdersList::moveUp(const Order& order){
-    vector<Order*>::iterator it = find_if(orders->begin(), orders->end(), [&order](const Order* q){ return q->getUniqueId() == order.getUniqueId();});
+void OrdersList::moveUp(const Order &order)
+{
+    vector<Order *>::iterator it =
+        find_if(orders->begin(), orders->end(), [&order](const Order *q) {
+            return q->getUniqueId() == order.getUniqueId();
+        });
 
-    if(it != orders->end()){
+    if (it != orders->end())
+    {
         int index = (it - orders->begin());
-        if(index != 0){
-            this->move(order, index-1);
+        if (index != 0)
+        {
+            this->move(order, index - 1);
         }
     }
 }
 
-void OrdersList::moveDown(const Order& order){
-    vector<Order*>::iterator it = find_if(orders->begin(), orders->end(), [&order](const Order* q){ return q->getUniqueId() == order.getUniqueId();});
+void OrdersList::moveDown(const Order &order)
+{
+    vector<Order *>::iterator it =
+        find_if(orders->begin(), orders->end(), [&order](const Order *q) {
+            return q->getUniqueId() == order.getUniqueId();
+        });
 
-    if(it != orders->end()){
+    if (it != orders->end())
+    {
         int index = (it - orders->begin());
-        if(static_cast<std::size_t>(index) != (orders->size()-1)){
-            this->move(order, index+1);
+        if (static_cast<std::size_t>(index) != (orders->size() - 1))
+        {
+            this->move(order, index + 1);
         }
     }
 }
 
-void OrdersList::remove(const Order& order){
-    for (auto it = orders->cbegin(); it != orders->cend(); ++it){
-        if(((*it)->getUniqueId()) == order.getUniqueId()){  
+void OrdersList::remove(const Order &order)
+{
+    for (auto it = orders->cbegin(); it != orders->cend(); ++it)
+    {
+        if (((*it)->getUniqueId()) == order.getUniqueId())
+        {
             orders->erase(it);
             break;
         }
     }
 }
 
-const vector<Order*>& OrdersList::getList(){ 
-    return *orders; 
+const vector<Order *> &OrdersList::getList()
+{
+    return *orders;
 }
 
 int Order::counter = 0;
 
-Order::Order(const Order &order): uniqueId(new int(*order.uniqueId)),
-                                  executed(new bool(*order.executed)),
-                                  description(new std::string(*order.description)),
-                                  effect(new std::string(*order.effect))
-{}
+Order::Order(const Order &order) :
+    uniqueId(new int(*order.uniqueId)),
+    executed(new bool(*order.executed)),
+    description(new std::string(*order.description)),
+    effect(new std::string(*order.effect))
+{
+}
 
-Order::~Order(){
+Order::~Order()
+{
     delete this->description;
     delete this->effect;
     delete this->uniqueId;
     delete this->executed;
 }
 
-Order& Order::operator = (const Order &order){
-    if(this == &order){ 
-        return *this; 
+Order &Order::operator=(const Order &order)
+{
+    if (this == &order)
+    {
+        return *this;
     }
 
     description = new string(*order.description);
@@ -117,151 +153,266 @@ Order& Order::operator = (const Order &order){
     uniqueId = new int(counter++);
 
     return *this;
-} 
-
-
-Order::Order(const string& description, const string& effect): uniqueId(new int(counter++)),description(new string(description)), effect(new string(effect)){}
-
-const string& Order::getDescription(){ 
-    return *description; 
 }
 
-const string& Order::getEffect(){ 
-    return *effect; 
+Order::Order(const string &description, const string &effect) :
+    uniqueId(new int(counter++)),
+    description(new string(description)),
+    effect(new string(effect))
+{
 }
 
-void Order::setExecutedStatus(bool status){ *executed = status; }
+const string &Order::getDescription()
+{
+    return *description;
+}
 
-const bool& Order::getExecutedStatus(){ return *executed; }
+const string &Order::getEffect()
+{
+    return *effect;
+}
 
-int Order::getUniqueId() const { return *uniqueId;}
+void Order::setExecutedStatus(bool status)
+{
+    *executed = status;
+}
 
-ostream & operator << (ostream &out, const Order &order) 
-{ 
-    out << *(order.description); 
-    if(*(order.executed)){
-         out << " -> Effect: " << *(order.effect); 
+const bool &Order::getExecutedStatus()
+{
+    return *executed;
+}
+
+int Order::getUniqueId() const
+{
+    return *uniqueId;
+}
+
+ostream &operator<<(ostream &out, const Order &order)
+{
+    out << *(order.description);
+    if (*(order.executed))
+    {
+        out << " -> Effect: " << *(order.effect);
     };
-    out << "\n"; 
-    return out; 
-} 
+    out << "\n";
+    return out;
+}
 
-Deploy::Deploy(const Deploy &order): Order(order){}
+Deploy::Deploy(const Deploy &order) : Order(order)
+{
+}
 
-Deploy& Deploy::operator = (const Deploy &order){
-    if (&order == this){
+Deploy &Deploy::operator=(const Deploy &order)
+{
+    if (&order == this)
+    {
         return *this;
     }
-    Order::operator= (order);
+    Order::operator=(order);
     return *this;
 }
 
-Deploy::Deploy():Order("Deploy", "Deploy troops to a territory"){}
+Deploy::Deploy() : Order("Deploy", "Deploy troops to a territory")
+{
+}
 
-Deploy::~Deploy(){}
+Deploy::~Deploy()
+{
+}
 
-void Deploy::validate(){}
+void Deploy::validate()
+{
+}
 
-void Deploy::execute(){ this->setExecutedStatus(true); } 
+void Deploy::execute()
+{
+    this->setExecutedStatus(true);
+}
 
-Order* Deploy::clone() const { return new Deploy(*this); }
+Order *Deploy::clone() const
+{
+    return new Deploy(*this);
+}
 
-Advance::Advance(const Advance &order): Order(order){}
+Advance::Advance(const Advance &order) : Order(order)
+{
+}
 
-Advance& Advance::operator = (const Advance &order){
-    if (&order == this){
+Advance &Advance::operator=(const Advance &order)
+{
+    if (&order == this)
+    {
         return *this;
     }
-    Order::operator= (order);
+    Order::operator=(order);
     return *this;
 }
 
-Advance::Advance():Order("Advance", "Advance troops to a neighbouring territory"){}
+Advance::Advance() : Order("Advance", "Advance troops to a neighbouring territory")
+{
+}
 
-Advance::~Advance(){}
+Advance::~Advance()
+{
+}
 
-void Advance::validate(){}
+void Advance::validate()
+{
+}
 
-void Advance::execute(){ this->setExecutedStatus(true); }
+void Advance::execute()
+{
+    this->setExecutedStatus(true);
+}
 
-Order* Advance::clone() const { return new Advance(*this); }
+Order *Advance::clone() const
+{
+    return new Advance(*this);
+}
 
-Bomb::Bomb(const Bomb &order): Order(order){}
+Bomb::Bomb(const Bomb &order) : Order(order)
+{
+}
 
-Bomb& Bomb::operator = (const Bomb &order){
-    if (&order == this){
+Bomb &Bomb::operator=(const Bomb &order)
+{
+    if (&order == this)
+    {
         return *this;
     }
-    Order::operator= (order);
+    Order::operator=(order);
     return *this;
 }
 
-Bomb::Bomb():Order("Bomb", "Bomb a territory"){}
+Bomb::Bomb() : Order("Bomb", "Bomb a territory")
+{
+}
 
-Bomb::~Bomb(){}
+Bomb::~Bomb()
+{
+}
 
-void Bomb::validate(){}
+void Bomb::validate()
+{
+}
 
-void Bomb::execute(){ this->setExecutedStatus(true); }
+void Bomb::execute()
+{
+    this->setExecutedStatus(true);
+}
 
-Order* Bomb::clone() const { return new Bomb(*this); }
+Order *Bomb::clone() const
+{
+    return new Bomb(*this);
+}
 
-Blockade::Blockade(const Blockade &order): Order(order){}
+Blockade::Blockade(const Blockade &order) : Order(order)
+{
+}
 
-Blockade& Blockade::operator = (const Blockade &order){
-    if (&order == this){
+Blockade &Blockade::operator=(const Blockade &order)
+{
+    if (&order == this)
+    {
         return *this;
     }
-    Order::operator= (order);
+    Order::operator=(order);
     return *this;
 }
 
-Blockade::Blockade():Order("Blockade", "Seals a territory, Prevents people or goods from entering or leaving the territory"){}
+Blockade::Blockade() :
+    Order("Blockade", "Seals a territory, Prevents people or goods from entering or "
+                      "leaving the territory")
+{
+}
 
-Blockade::~Blockade(){}
+Blockade::~Blockade()
+{
+}
 
-void Blockade::validate(){}
+void Blockade::validate()
+{
+}
 
-void Blockade::execute(){ this->setExecutedStatus(true); }
+void Blockade::execute()
+{
+    this->setExecutedStatus(true);
+}
 
-Order* Blockade::clone() const { return new Blockade(*this); }
+Order *Blockade::clone() const
+{
+    return new Blockade(*this);
+}
 
-Airlift::Airlift(const Airlift &order): Order(order){}
+Airlift::Airlift(const Airlift &order) : Order(order)
+{
+}
 
-Airlift& Airlift::operator = (const Airlift &order){
-    if (&order == this){
+Airlift &Airlift::operator=(const Airlift &order)
+{
+    if (&order == this)
+    {
         return *this;
     }
-    Order::operator= (order);
+    Order::operator=(order);
     return *this;
 }
 
-Airlift::Airlift():Order("Airlift", "Transport suplies or troops by air"){}
+Airlift::Airlift() : Order("Airlift", "Transport suplies or troops by air")
+{
+}
 
-Airlift::~Airlift(){}
+Airlift::~Airlift()
+{
+}
 
-void Airlift::validate(){}
+void Airlift::validate()
+{
+}
 
-void Airlift::execute(){ this->setExecutedStatus(true); }
+void Airlift::execute()
+{
+    this->setExecutedStatus(true);
+}
 
-Order* Airlift::clone() const { return new Airlift(*this); }
+Order *Airlift::clone() const
+{
+    return new Airlift(*this);
+}
 
-Negotiate::Negotiate(const Negotiate &order): Order(order){}
+Negotiate::Negotiate(const Negotiate &order) : Order(order)
+{
+}
 
-Negotiate& Negotiate::operator = (const Negotiate &order){
-    if (&order == this){
+Negotiate &Negotiate::operator=(const Negotiate &order)
+{
+    if (&order == this)
+    {
         return *this;
     }
-    Order::operator= (order);
+    Order::operator=(order);
     return *this;
 }
 
-Negotiate::Negotiate():Order("Negotiate", "Negotiate with the opposition to reach an agreement"){}
+Negotiate::Negotiate() :
+    Order("Negotiate", "Negotiate with the opposition to reach an agreement")
+{
+}
 
-Negotiate::~Negotiate(){}
+Negotiate::~Negotiate()
+{
+}
 
-void Negotiate::validate(){}
+void Negotiate::validate()
+{
+}
 
-void Negotiate::execute(){ this->setExecutedStatus(true); }
+void Negotiate::execute()
+{
+    this->setExecutedStatus(true);
+}
 
-Order* Negotiate::clone() const { return new Negotiate(*this); }
+Order *Negotiate::clone() const
+{
+    return new Negotiate(*this);
+}
