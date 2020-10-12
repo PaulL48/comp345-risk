9668312d98915779ac9fd17c4abfe278ff754bbe
Paul Lane <paul.lane083@gmail.com>
2020-10-12 02:57:11 -0400

    Fix main() problem

--- src/MapLoaderDriver.cpp
+++ src/MapLoaderDriver.cpp
@@ -1,23 +1,23 @@
-//
-// Created by Ruslan Dallin on 2020-09-20.
-//
+// //
+// // Created by Ruslan Dallin on 2020-09-20.
+// //
 
-#include "MapLoader.h"
-#include <filesystem>
-#include <fstream>
-#include <iostream>
-#include <sstream>
-#include <vector>
+// #include "MapLoader.h"
+// #include <filesystem>
+// #include <fstream>
+// #include <iostream>
+// #include <sstream>
+// #include <vector>
 
-int main()
-{
-    for (auto &directoryEntry : std::filesystem::directory_iterator("./maps"))
-    {
-        std::string fileName = directoryEntry.path().string();
-        Map map = MapLoader::loadMap(fileName);
-        std::cout << "Map file \"" << fileName
-                  << "\" status: " << map.getErrorString(map.validate()) << "\n";
-        std::cout << "\n";
-    }
-    return 0;
-}
+// int main()
+// {
+//     for (auto &directoryEntry : std::filesystem::directory_iterator("./maps"))
+//     {
+//         std::string fileName = directoryEntry.path().string();
+//         Map map = MapLoader::loadMap(fileName);
+//         std::cout << "Map file \"" << fileName
+//                   << "\" status: " << map.getErrorString(map.validate()) << "\n";
+//         std::cout << "\n";
+//     }
+//     return 0;
+// }
