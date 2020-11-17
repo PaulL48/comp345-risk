a97bfdb7d009a30e2fb1ec3c127fe07d8abefd69
chaoukic <sammychaouki25@gmail.com>
2020-11-17 02:11:32 -0500

    Implement functionalities to Gamestartup

--- src/GameStartup.cpp
+++ src/GameStartup.cpp
@@ -43,26 +43,78 @@ int GameStartup::getNumberOfArmies(int numPlayer){
 void GameStartup::startupPhase(){
     int numArmies = getNumberOfArmies(numPlayers);
     *this->players = intializePlayers(numArmies);
+    *this->orderPlayers = intitializeOrderPlayers(numPlayers);
     return;
+}
+std::vector<int>& GameStartup::intitializeOrderPlayers(const int numPlayers){
+     std::vector<int>* orderPlayers = new std::vector<int>(numPlayers,10);
+     std::vector<int> numTerritoriesOwned = std::vector<int>(numPlayers,0);
+     for(int i = 0; i < numTerritoriesOwned.size(); i++){
+         numTerritoriesOwned.push_back(i);
+     }
+     int index= 0;
+     for(int i = 0 ; i < orderPlayers->size();i ++){
+         index = (rand() % numTerritoriesOwned.size());
+         int val = numTerritoriesOwned.at(index);
+         orderPlayers->at(i) = val;
+     }
+     return *orderPlayers;
 }
 
 
-
- std::vector<Player>& GameStartup::intializePlayers(int numArmies){
+std::vector<Player>& GameStartup::intializePlayers(const int numArmies){
+    Graph<Territory> territories = map->getGraph();
     std::vector<Player>* playerList  = new std::vector<Player>(numPlayers);
-    std::vector<Territory>* territoriesLeft = new std::vector<Territory>();
+    std::vector<int> numTerritoriesOwned = std::vector<int>(numPlayers,0);
+    int territoriesLeft = territories.size();
+    int territoriesPerPlayer = territoriesLeft/numPlayers;
     for (int i = 0; i < numPlayers; i++){
         std::string *playerName = new std::string("Player " + std::to_string(i + 1));
         Hand hand;
         OrdersList orderlist;
         std::vector<Territory>* toAttack = new std::vector<Territory>();
         std::vector<Territory>* toDefend = new std::vector<Territory>();
-        int index = (rand() % territoriesLeft->size());
-        Territory toDef = territoriesLeft->at(index);
-        toDefend->push_back(toDef);
-        toAttack->erase(std::remove(toAttack->begin(), toAttack->end(), toDef), toAttack->end());
         Player p = Player(*playerName,*toAttack,*toDefend, hand, orderlist,numArmies);
         playerList->push_back(p);
     }
+    std::vector<int> numTerritoriesOwned = std::vector<int>(numPlayers);
+    int index= 0;
+    for(Territory territory: territories ){
+        bool validInsert = true;
+        
+        while(validInsert){
+            index = (rand() % numPlayers);
+            if(territoriesLeft == 1 && territories.size() % numPlayers != 0 ){
+                territory.setPlayer(playerList->at(index));
+                validInsert= false;
+                territoriesLeft--;
+            }else{
+                if(numTerritoriesOwned.at(index) != territoriesPerPlayer){
+                    int val = numTerritoriesOwned.at(index) + 1;
+                    territory.setPlayer(playerList->at(index));
+                    numTerritoriesOwned.at(index) = val;
+                    territoriesLeft--;
+                    validInsert=false;
+                }
+            }
+        }
+    }
+    for (int i = 0; i < numPlayers; i++){
+        Player player = playerList->at(i);
+        std::vector<Territory> ownedTerritories = map->getPlayersTerritories(player);
+        for(Territory territory: ownedTerritories){
+            std::unordered_set<Territory> neighborTerritories = *map->getNeighbors(territory);
+            for(Territory neighborTerritory : neighborTerritories){
+                if( !(neighborTerritory.getOwningPlayer() == player)){
+                    player.toDefend().push_back(territory);
+                    if(!(std::find(player.toAttack().begin(), player.toAttack().end(), neighborTerritory) != player.toAttack().end())) {
+                         player.toAttack().push_back(neighborTerritory);
+                    }
+                }
+            }
+        }
+        playerList->at(i) = player;
+    }
+
     return *playerList;
  }
