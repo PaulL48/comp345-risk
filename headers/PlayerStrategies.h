7e16bd246abb9245a7850b581c26b0826868d9f3
Paul Lane <paul.lane083@gmail.com>
2020-11-23 20:33:01 -0500

    Adding player strategy templates

--- headers/PlayerStrategies.h
+++ headers/PlayerStrategies.h
@@ -6,41 +6,46 @@
 class PlayerStrategy
 {
 public:
-    virtual void issueOrder() = 0;
-    virtual std::vector<Territory>& toAttack() = 0;
-    virtual std::vector<Territory>& toDefend() = 0;
+    // Fill the order list of the player with orders based on the intended strategy behavior
+    virtual void issueOrder(const Map& map, Player& player) = 0;
+
+    // Return the list of territories to attack based on the intended strategy behavior
+    virtual std::vector<Territory>& toAttack(const Map& map, Player& player) = 0;
+
+    // Return the list of territories to reinforce based on the intended strategy behavior
+    virtual std::vector<Territory>& toDefend(const Map& map, Player& player) = 0;
 };
 
 class HumanPlayerStrategy : public PlayerStrategy
 {
 public:
-    virtual void issueOrder();
-    virtual std::vector<Territory>& toAttack();
-    virtual std::vector<Territory>& toDefend();
+    virtual void issueOrder(const Map& map, Player& player);
+    virtual std::vector<Territory>& toAttack(const Map& map, Player& player);
+    virtual std::vector<Territory>& toDefend(const Map& map, Player& player);
 };
 
 class AggressivePlayerStrategy : public PlayerStrategy
 {
 public:
-    virtual void issueOrder();
-    virtual std::vector<Territory>& toAttack();
-    virtual std::vector<Territory>& toDefend();
+    virtual void issueOrder(const Map& map, Player& player);
+    virtual std::vector<Territory>& toAttack(const Map& map, Player& player);
+    virtual std::vector<Territory>& toDefend(const Map& map, Player& player);
 };
 
 class BenevolentPlayerStrategy : public PlayerStrategy
 {
 public:
-    virtual void issueOrder();
-    virtual std::vector<Territory>& toAttack();
-    virtual std::vector<Territory>& toDefend();
+    virtual void issueOrder(const Map& map, Player& player);
+    virtual std::vector<Territory>& toAttack(const Map& map, Player& player);
+    virtual std::vector<Territory>& toDefend(const Map& map, Player& player);
 };
 
 class NeutralPlayerStrategy : public PlayerStrategy
 {
 public:
-    virtual void issueOrder();
-    virtual std::vector<Territory>& toAttack();
-    virtual std::vector<Territory>& toDefend();
+    virtual void issueOrder(const Map& map, Player& player);
+    virtual std::vector<Territory>& toAttack(const Map& map, Player& player);
+    virtual std::vector<Territory>& toDefend(const Map& map, Player& player);
 };
 
 #endif
--- src/PlayerStrategies.cpp
+++ src/PlayerStrategies.cpp
@@ -5,19 +5,74 @@
 // CLASS DEFINITIONS: HumanPlayerStrategy
 //============================================================================================================================================================
 
+void HumanPlayerStrategy::issueOrder(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& HumanPlayerStrategy::toAttack(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& HumanPlayerStrategy::toDefend(const Map& map, Player& player)
+{
+
+}
 
 //============================================================================================================================================================
 // CLASS DEFINITIONS: AggressivePlayerStrategy
 //============================================================================================================================================================
 
+void AggressivePlayerStrategy::issueOrder(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& AggressivePlayerStrategy::toAttack(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& AggressivePlayerStrategy::toDefend(const Map& map, Player& player)
+{
+
+}
 
 //============================================================================================================================================================
 // CLASS DEFINITIONS: BenevolentPlayerStrategy
 //============================================================================================================================================================
 
+void BenevolentPlayerStrategy::issueOrder(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& BenevolentPlayerStrategy::toAttack(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& BenevolentPlayerStrategy::toDefend(const Map& map, Player& player)
+{
+    
+}
 
 //============================================================================================================================================================
 // CLASS DEFINITIONS: NeutralPlayerStrategy
 //============================================================================================================================================================
 
+void NeutralPlayerStrategy::issueOrder(const Map& map, Player& player)
+{
 
+}
+
+std::vector<Territory>& NeutralPlayerStrategy::toAttack(const Map& map, Player& player)
+{
+
+}
+
+std::vector<Territory>& NeutralPlayerStrategy::toDefend(const Map& map, Player& player)
+{
+
+}
